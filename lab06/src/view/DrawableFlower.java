3920122c20f6dd41369cc54c4713bd3707eb2dc1
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2016-12-06 19:35:00 +0100

    added interface to represent Flower-thread properties avaliable for view

---
+++ lab06/src/view/DrawableFlower.java
@@ -0,0 +1,8 @@
+package view;
+
+public interface DrawableFlower {
+	public float getHydrationLevel();
+	
+	public void setDehydrationLimit(float dehydrationLimit);
+	public void setDehydrationStep(float dehydrationStep);
+}
